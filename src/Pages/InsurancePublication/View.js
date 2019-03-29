c341b2f1d91ecae8e1e0a7280d126c37ac21c19d
Soulike <316413310@qq.com>
2019-03-29 20:29:01 +0800

    完成 保险发布 组件拆分

--- src/Pages/InsurancePublication/View.js
+++ src/Pages/InsurancePublication/View.js
@@ -5,54 +5,50 @@ import Card from '../../Components/Card';
 import {PAGE_ID_TO_ROUTE, REQUIRE_LOGIN_PAGE_ID} from '../../Config';
 import {browserHistory} from 'react-router';
 
-class InsurancePublication extends React.Component
+function InsurancePublication()
 {
-    onCancelButtonClick = () =>
-    {
-        browserHistory.push(PAGE_ID_TO_ROUTE[REQUIRE_LOGIN_PAGE_ID.INSURANCE_COMPANY_INSURANCE_LIST]);
-    };
-
     //TODO：项目未确定，后台待补充
-    render()
-    {
-        return (
-            <div className={Style.InsurancePublication}>
-                <Card className={Style.contentWrapper}>
-                    <Title>新建保险</Title>
-                    <div className={Style.formWrapper}>
-                        <label className={Style.inputWrapper}>
-                            <span className={Style.label}>保险名称：</span>
-                            <input type="text" />
-                        </label>
-                        <label className={Style.inputWrapper}>
-                            <span className={Style.label}>保额：</span>
-                            <input type="text" />
-                        </label>
-                        <label className={Style.inputWrapper}>
-                            <span className={Style.label}>承保年龄：</span>
-                            <input type="text" />
-                        </label>
-                        <label className={Style.inputWrapper}>
-                            <span className={Style.label}>保障期限：</span>
-                            <input type="text" />
-                        </label>
-                        <label className={Style.inputWrapper}>
-                            <span className={Style.label}>保费价格：</span>
-                            <input type="text" />
-                        </label>
-                        <label className={Style.inputWrapper}>
-                            <span className={Style.label}>保险详情：</span>
-                            <textarea className={Style.insuranceDetailTextArea} />
-                        </label>
-                    </div>
-                </Card>
-                <div className={Style.buttonGroup} role="group">
-                    <button type="button" className={Style.cancelButton} onClick={this.onCancelButtonClick}>取消</button>
-                    <button type="button" className={Style.publishButton}>发布</button>
+    return (
+        <div className={Style.InsurancePublication}>
+            <Card className={Style.contentWrapper}>
+                <Title>新建保险</Title>
+                <div className={Style.formWrapper}>
+                    <label className={Style.inputWrapper}>
+                        <span className={Style.label}>保险名称：</span>
+                        <input type="text" />
+                    </label>
+                    <label className={Style.inputWrapper}>
+                        <span className={Style.label}>保额：</span>
+                        <input type="text" />
+                    </label>
+                    <label className={Style.inputWrapper}>
+                        <span className={Style.label}>承保年龄：</span>
+                        <input type="text" />
+                    </label>
+                    <label className={Style.inputWrapper}>
+                        <span className={Style.label}>保障期限：</span>
+                        <input type="text" />
+                    </label>
+                    <label className={Style.inputWrapper}>
+                        <span className={Style.label}>保费价格：</span>
+                        <input type="text" />
+                    </label>
+                    <label className={Style.inputWrapper}>
+                        <span className={Style.label}>保险详情：</span>
+                        <textarea className={Style.insuranceDetailTextArea} />
+                    </label>
                 </div>
+            </Card>
+            <div className={Style.buttonGroup} role="group">
+                <button type="button" className={Style.cancelButton} onClick={() =>
+                {
+                    browserHistory.push(PAGE_ID_TO_ROUTE[REQUIRE_LOGIN_PAGE_ID.INSURANCE_COMPANY_INSURANCE_LIST]);
+                }}>取消
+                </button>
+                <button type="button" className={Style.publishButton}>发布</button>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default InsurancePublication;
