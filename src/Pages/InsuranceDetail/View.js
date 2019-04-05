fe1cbc1e4f802e82901f9e1f14f993358905664f
Soulike <316413310@qq.com>
2019-04-05 13:55:50 +0800

    完成保险详情页面获取数据逻辑

--- src/Pages/InsuranceDetail/View.js
+++ src/Pages/InsuranceDetail/View.js
@@ -4,10 +4,23 @@ import PropTypes from 'prop-types';
 import Card from '../../Components/Card';
 import Icon from 'antd/lib/icon';
 import Radio from 'antd/lib/radio';
+import Skeleton from 'antd/lib/skeleton';
 
 function InsuranceDetail(props)
 {
-    const {insuranceImageSrc, insuranceName} = props;
+    const {
+        hasGotInfo,
+        insuranceImageSrc,
+        insuranceName,
+        isSpecialMedicalCare,
+        hasSocialSecurity,
+        insuranceAmount,
+        insurancePeriod,
+        insuranceDiseaseType,
+        coveringAge,
+        salesArea,
+        insurancePrice,
+    } = props;
     return (
         <div className={Style.InsuranceDetail}>
             <Card className={Style.insuranceDetailContainer}>
@@ -15,71 +28,73 @@ function InsuranceDetail(props)
                     <img src={insuranceImageSrc} alt="insuranceImage" className={Style.image} />
                 </div>
                 <div className={Style.infoWrapper}>
-                    <div className={Style.info}>
-                        <div className={Style.insuranceNameWrapper}>
-                            <div className={Style.insuranceName}>
+                    <Skeleton active={true} loading={!hasGotInfo}>
+                        <div className={Style.info}>
+                            <div className={Style.insuranceNameWrapper}>
+                                <div className={Style.insuranceName}>
                                 <span className={Style.icon}>
                                     <Icon type="heart" theme="twoTone" twoToneColor={'#F00'} />
                                 </span>
-                                {insuranceName}
+                                    {insuranceName}
+                                </div>
                             </div>
-                        </div>
-                        <div className={Style.itemWrapper}>
-                            <div className={Style.label}>特需医疗</div>
-                            <div className={Style.itemContent}>
-                                <Radio.Group defaultValue={false} disabled={true}>
-                                    <Radio value={true}>包含</Radio>
-                                    <Radio value={false}>不包含</Radio>
-                                </Radio.Group>
+                            <div className={Style.itemWrapper}>
+                                <div className={Style.label}>特需医疗</div>
+                                <div className={Style.itemContent}>
+                                    <Radio.Group defaultValue={!!isSpecialMedicalCare} disabled={true}>
+                                        <Radio value={true}>包含</Radio>
+                                        <Radio value={false}>不包含</Radio>
+                                    </Radio.Group>
+                                </div>
                             </div>
-                        </div>
-                        <div className={Style.itemWrapper}>
-                            <div className={Style.label}>有无社保</div>
-                            <div className={Style.itemContent}>
-                                <Radio.Group defaultValue={false} disabled={true}>
-                                    <Radio value={true}>有</Radio>
-                                    <Radio value={false}>无</Radio>
-                                </Radio.Group>
+                            <div className={Style.itemWrapper}>
+                                <div className={Style.label}>有无社保</div>
+                                <div className={Style.itemContent}>
+                                    <Radio.Group defaultValue={!!hasSocialSecurity} disabled={true}>
+                                        <Radio value={true}>有</Radio>
+                                        <Radio value={false}>无</Radio>
+                                    </Radio.Group>
+                                </div>
                             </div>
-                        </div>
-                        <div className={Style.itemWrapper}>
-                            <div className={Style.label}>保额</div>
-                            <div className={Style.itemContent}>
-                                100 万
+                            <div className={Style.itemWrapper}>
+                                <div className={Style.label}>保额</div>
+                                <div className={Style.itemContent}>
+                                    {insuranceAmount} 元
+                                </div>
                             </div>
-                        </div>
-                        <div className={Style.itemWrapper}>
-                            <div className={Style.label}>保障期限</div>
-                            <div className={Style.itemContent}>
-                                1 年
+                            <div className={Style.itemWrapper}>
+                                <div className={Style.label}>保障期限</div>
+                                <div className={Style.itemContent}>
+                                    {insurancePeriod}
+                                </div>
                             </div>
-                        </div>
-                        <div className={Style.itemWrapper}>
-                            <div className={Style.label}>保险病种</div>
-                            <div className={Style.itemContent}>
-                                X 种常见疾病 Y 种重大疾病
+                            <div className={Style.itemWrapper}>
+                                <div className={Style.label}>保险病种</div>
+                                <div className={Style.itemContent}>
+                                    {insuranceDiseaseType}
+                                </div>
                             </div>
-                        </div>
-                        <div className={Style.itemWrapper}>
-                            <div className={Style.label}>承保年龄</div>
-                            <div className={Style.itemContent}>
-                                出生满 90 天至 60 周岁人群
+                            <div className={Style.itemWrapper}>
+                                <div className={Style.label}>承保年龄</div>
+                                <div className={Style.itemContent}>
+                                    {coveringAge}
+                                </div>
                             </div>
-                        </div>
-                        <div className={Style.itemWrapper}>
-                            <div className={Style.label}>销售区域</div>
-                            <div className={Style.itemContent}>
-                                全国
+                            <div className={Style.itemWrapper}>
+                                <div className={Style.label}>销售区域</div>
+                                <div className={Style.itemContent}>
+                                    {salesArea}
+                                </div>
                             </div>
-                        </div>
-                        <div className={Style.itemWrapper}>
-                            <div className={Style.label}>保费价格</div>
-                            <div className={Style.itemContent}>
-                                ￥ <span className={Style.insurancePrice}>96/人</span>
+                            <div className={Style.itemWrapper}>
+                                <div className={Style.label}>保费价格</div>
+                                <div className={Style.itemContent}>
+                                    <span className={Style.insurancePrice}>{insurancePrice}</span>
+                                </div>
                             </div>
+                            <div className={Style.buttonWrapper} />
                         </div>
-                        <div className={Style.buttonWrapper} />
-                    </div>
+                    </Skeleton>
                 </div>
             </Card>
         </div>
@@ -87,12 +102,17 @@ function InsuranceDetail(props)
 }
 
 InsuranceDetail.propTypes = {
+    hasGotInfo: PropTypes.bool.isRequired,
     insuranceImageSrc: PropTypes.string.isRequired,
     insuranceName: PropTypes.string.isRequired,
-};
-
-InsuranceDetail.defaultProps = {
-    insuranceName: '保险名称',
+    isSpecialMedicalCare: PropTypes.number.isRequired,   // 是否是特殊医疗，0 或 1
+    hasSocialSecurity: PropTypes.number.isRequired,      // 有无社保，0 或 1
+    insuranceAmount: PropTypes.number.isRequired,        // 保额，单位是人民币元
+    insurancePeriod: PropTypes.string.isRequired,        // 保险期限
+    insuranceDiseaseType: PropTypes.string.isRequired,   // 保险病种
+    coveringAge: PropTypes.string.isRequired,            // 承保年龄
+    salesArea: PropTypes.string.isRequired,              // 销售区域
+    insurancePrice: PropTypes.number.isRequired,         // 保费价格，单位是人民币元
 };
 
 export default InsuranceDetail;
